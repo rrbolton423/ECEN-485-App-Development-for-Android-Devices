6eac1564cb2cd8b0bf899a9408ec1f9db43cf171
Romell Bolton <user.email>
2018-03-26 16:16:49 -0400

    Update Android project in Repository

--- ServiceApp/app/src/main/java/com/romellbolton/serviceapp/MainActivity.java
+++ ServiceApp/app/src/main/java/com/romellbolton/serviceapp/MainActivity.java
@@ -11,11 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
+    // onClick() starts the Service
     public void onClick(View view) {
+
+        // Create an Intent to the Service class
         Intent intent = new Intent(this, MyService.class);
+
+        // Start the Service, passing the Intent
         startService(intent);
     }
 }
