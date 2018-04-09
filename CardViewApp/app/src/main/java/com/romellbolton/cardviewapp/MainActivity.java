e566792d616f8aea35ee9b5d757ad013348df485
Romell Bolton <user.email>
2018-04-09 13:51:14 -0400

    Add Android project to Repository

---
+++ CardViewApp/app/src/main/java/com/romellbolton/cardviewapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.romellbolton.cardviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
